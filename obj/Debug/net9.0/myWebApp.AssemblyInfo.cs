5ab409f8376b1d3713d71e3f16e5f7b13c97039d
Marcin Dziurdzia <marcin.dziurdzia99@gmail.com>
2025-01-22 18:33:11 +0100

    updated welcome page.

--- obj/Debug/net9.0/myWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/myWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79f80b67acb0cae8547f40b419cdd7efdd2d309")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3cb07c7d8ec81125273cc5464bfbc38e5a62918")]
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
