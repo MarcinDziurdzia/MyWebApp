fc59bac82943b717be162e0ded9f495d2613e0d8
Marcin Dziurdzia <marcin.dziurdzia99@gmail.com>
2025-01-22 18:26:06 +0100

    Adding gitignore

--- obj/Debug/net9.0/myWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/myWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3029ed2aa17cdc82f9a9e5f8fe4132f02ebde81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79f80b67acb0cae8547f40b419cdd7efdd2d309")]
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
